6435f9a94faf152e773de6b16cde5725448fb0ee
Jose Aguilar <aguilar.jose11@outlook.com>
2018-12-18 13:14:46 -0600

    menu module created.

---
+++ menus.py
@@ -0,0 +1,17 @@
+# menu module
+
+# abstract class. do not define. leave AS IT IS.
+class menu:
+    # should return a String with the final choice.
+    def getchoice(choice):
+        pass
+    # should return a number with the choice number for menu.
+    def menu():
+        pass
+
+class menurunner:
+    # menu should be from menu class
+    def __init__(self):
+        pass
+    def runmenu(menu):
+        pass
--- timesheet.py
+++ timesheet.py
@@ -1,13 +1,9 @@
+# timesheet main program.
 
-
-
-
-
-
-
-
+def launch():
+    pass
 
 
 # main runner (No touchy!)
 if __name__ == "__main__":
-    main()
+    launch()
