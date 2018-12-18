a629b27a568b5585a545a896e2dbb4871b180440
Jose Aguilar <aguilar.jose11@outlook.com>
2018-12-18 14:28:18 -0600

    fixed class missing 'self' for methods.

--- menu.py
+++ menu.py
@@ -2,13 +2,13 @@
 
 # abstract class. do not define. leave AS IT IS.
 class Menu:
-    def __init__():
+    def __init__(self):
         pass
     # returns a String with the final choice.
-    def getchoice(choice):
+    def getchoice(self):
         pass
     # print out entire menu ONCE.
-    def menu():
+    def menu(self):
         pass
 
 # this method should be fully defined.
@@ -16,5 +16,7 @@ def runmenu(exec):
     exec.menu()
     choice = 0
     while(not(type(choice) == str)):
+        # testing line
+        input("Click enter to continue . . . (will crash if enter)")
         choice = exec.getchoice()
     return choice
